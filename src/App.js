99aba0f3a75241d2582154bdc257fd94243bfe64
yuyatsunazawa <36617202+yuyatsunazawa@users.noreply.github.com>
2025-03-12 00:14:05 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import React from "react";
+import StrategySelection from "./components/StrategySelection";
+import MarketData from "./components/MarketData";
+
+function App() {
+  return (
+    <div>
+      <h1>Investment UI</h1>
+      <MarketData />
+      <StrategySelection />
+    </div>
+  );
+}
+
+export default App;
 
